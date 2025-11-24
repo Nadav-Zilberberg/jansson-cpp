f37429c31e99ca1df7a4f0071c5a9fffe362fb24
test <test@example.com>
2025-11-24 09:46:51 +0000

    A concise summary of the fix

--- jansson-cpp/src/memory.cpp
+++ jansson-cpp/src/memory.cpp
@@ -6,53 +6,55 @@
  * under the terms of the MIT license. See LICENSE for details.
  */
 
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
+#include <memory>
 
 #include "jansson.hpp"
-#include "jansson_private.hpp"
 
 /* C89 allows these to be macros */
 #undef malloc
 #undef realloc
 #undef free
 
-/* memory function pointers */
-static json_malloc_t do_malloc = malloc;
-static json_realloc_t do_realloc = realloc;
-static json_free_t do_free = free;
+namespace jansson {
 
-void *static_cast<char*>(jsonp_malloc(size_t size) {
+/* memory function pointers */
+static json_malloc_t do_malloc = [](size_t size) -> void* { return std::malloc(size); };
+static json_realloc_t do_realloc = [](void* ptr, size_t size) -> void* { return std::realloc(ptr, size); };
+static json_free_t do_free = [](void* ptr) { std::free(ptr); };
+
+void* jsonp_malloc(size_t size) {
     if (!size)
-        return NULL;
+        return nullptr;
 
     return (*do_malloc)(size);
 }
 
-void jsonp_free(void *ptr) {
+void jsonp_free(void* ptr) {
     if (!ptr)
         return;
 
     (*do_free)(ptr);
 }
 
-void *static_cast<char*>(jsonp_realloc(void *ptr, size_t originalSize, size_t newSize) {
-    void *newMemory;
+void* jsonp_realloc(void* ptr, size_t originalSize, size_t newSize) {
+    void* newMemory;
 
     if (do_realloc)
         return (*do_realloc)(ptr, newSize);
 
     // realloc emulation using malloc and free
     if (newSize == 0) {
-        if (ptr != NULL)
+        if (ptr != nullptr)
             (*do_free)(ptr);
 
-        return NULL;
+        return nullptr;
     } else {
         newMemory = (*do_malloc)(newSize);
 
-        if ((newMemory != NULL) && (ptr != NULL)) {
-            memcpy(newMemory, ptr, (originalSize < newSize) ? originalSize : newSize);
+        if ((newMemory != nullptr) && (ptr != nullptr)) {
+            std::memcpy(newMemory, ptr, (originalSize < newSize) ? originalSize : newSize);
 
             (*do_free)(ptr);
         }
@@ -61,21 +63,21 @@ void *static_cast<char*>(jsonp_realloc(void *ptr, size_t originalSize, size_t ne
     }
 }
 
-char *jsonp_strndup(const char *str, size_t len) {
-    char *new_str;
+char* jsonp_strndup(const char* str, size_t len) {
+    char* new_str;
 
-    new_str = static_cast<char*>(jsonp_malloc(len + 1);
+    new_str = static_cast<char*>(jsonp_malloc(len + 1));
     if (!new_str)
-        return NULL;
+        return nullptr;
 
-    memcpy(new_str, str, len);
+    std::memcpy(new_str, str, len);
     new_str[len] = '\0';
     return new_str;
 }
 
 void json_set_alloc_funcs(json_malloc_t malloc_fn, json_free_t free_fn) {
     do_malloc = malloc_fn;
-    do_realloc = NULL;
+    do_realloc = nullptr;
     do_free = free_fn;
 }
 
@@ -86,14 +88,15 @@ void json_set_alloc_funcs2(json_malloc_t malloc_fn, json_realloc_t realloc_fn,
     do_free = free_fn;
 }
 
-void json_get_alloc_funcs(json_malloc_t *malloc_fn, json_free_t *free_fn) {
+void json_get_alloc_funcs(json_malloc_t* malloc_fn, json_free_t* free_fn) {
     if (malloc_fn)
         *malloc_fn = do_malloc;
     if (free_fn)
         *free_fn = do_free;
 }
-void json_get_alloc_funcs2(json_malloc_t *malloc_fn, json_realloc_t *realloc_fn,
-                           json_free_t *free_fn) {
+
+void json_get_alloc_funcs2(json_malloc_t* malloc_fn, json_realloc_t* realloc_fn,
+                           json_free_t* free_fn) {
     if (malloc_fn)
         *malloc_fn = do_malloc;
     if (realloc_fn)
@@ -101,3 +104,5 @@ void json_get_alloc_funcs2(json_malloc_t *malloc_fn, json_realloc_t *realloc_fn,
     if (free_fn)
         *free_fn = do_free;
 }
+
+} // namespace jansson
