9b4747918789cf0b3581ccc92a58cce54c688820
test <test@example.com>
2025-11-24 08:19:08 +0000

    A concise summary of the fix

--- jansson-cpp/include/jansson_config.hpp
+++ jansson-cpp/include/jansson_config.hpp
@@ -1,213 +1,136 @@
+/*
+ * Copyright (c) 2010-2016 Petri Lehtinen <petri@digip.org>
+ *
+ * Jansson is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ *
+ *
+ * This file specifies a part of the site-specific configuration for
+ * Jansson, namely those things that affect the public API in
+ * jansson.h.
+ *
+ * The CMake system will generate the jansson_config.h file and
+ * copy it to the build and install directories.
+ */
+
 #ifndef JANSSON_CONFIG_HPP
 #define JANSSON_CONFIG_HPP
 
-/* Define to 1 if you have the <endian.h> header file. */
+#include <cstdint>
+#include <cstddef>
+#include <climits>
+
+namespace jansson {
+namespace config {
+
+// Header availability configuration
+constexpr bool have_endian_h = true;
+constexpr bool have_fcntl_h = true;
+constexpr bool have_inttypes_h = true;
+constexpr bool have_locale_h = true;
+constexpr bool have_memory_h = true;
+constexpr bool have_sched_h = true;
+constexpr bool have_stdint_h = true;
+constexpr bool have_stdlib_h = true;
+constexpr bool have_strings_h = true;
+constexpr bool have_string_h = true;
+constexpr bool have_sys_param_h = true;
+constexpr bool have_sys_stat_h = true;
+constexpr bool have_sys_time_h = true;
+constexpr bool have_sys_types_h = true;
+constexpr bool have_unistd_h = true;
+
+// Function availability configuration
+constexpr bool have_close = true;
+constexpr bool have_getpid = true;
+constexpr bool have_gettimeofday = true;
+constexpr bool have_open = true;
+constexpr bool have_read = true;
+constexpr bool have_sched_yield = true;
+constexpr bool have_setlocale = true;
+constexpr bool have_strtoq = true;
+constexpr bool have_strtoll = true;
+
+// Atomic operations support
+constexpr bool have_atomic_builtins = true;
+constexpr bool have_sync_builtins = true;
+constexpr bool have_stdatomic_h = true;
+
+// Type sizes
+constexpr int sizeof_int = 4;
+constexpr int sizeof_int32_t = 4;
+constexpr int sizeof_int64_t = 8;
+constexpr int sizeof_long = 8;
+constexpr int sizeof_long_long = 8;
+constexpr int sizeof_short = 2;
+constexpr int sizeof_size_t = 8;
+constexpr int sizeof_ssize_t = 8;
+constexpr int sizeof_uint16_t = 2;
+constexpr int sizeof_uint32_t = 4;
+constexpr int sizeof_uint8_t = 1;
+
+// Package information
+constexpr const char* package_name = "jansson";
+constexpr const char* package_version = "2.14";
+constexpr const char* package_string = "jansson 2.14";
+constexpr const char* version = "2.14";
+
+// Thread safety configuration
+constexpr bool thread_safe_refcount = have_atomic_builtins || have_sync_builtins;
+
+} // namespace config
+} // namespace jansson
+
+// Compatibility macros for backward compatibility
+// These are kept for existing code that uses the macro names
 #define HAVE_ENDIAN_H 1
-
-/* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
-
-/* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the <locale.h> header file. */
 #define HAVE_LOCALE_H 1
-
-/* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the <sched.h> header file. */
 #define HAVE_SCHED_H 1
-
-/* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/param.h> header file. */
 #define HAVE_SYS_PARAM_H 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/time.h> header file. */
 #define HAVE_SYS_TIME_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* Define to 1 if you have the `_strtoi64' function. */
-/* #undef HAVE__STRTOI64 */
-
-/* Define to 1 if you have the `close' function. */
 #define HAVE_CLOSE 1
-
-/* Define to 1 if you have the `getpid' function. */
 #define HAVE_GETPID 1
-
-/* Define to 1 if you have the `gettimeofday' function. */
 #define HAVE_GETTIMEOFDAY 1
-
-/* Define to 1 if you have the `open' function. */
 #define HAVE_OPEN 1
-
-/* Define to 1 if you have the `read' function. */
 #define HAVE_READ 1
-
-/* Define to 1 if you have the `sched_yield' function. */
 #define HAVE_SCHED_YIELD 1
-
-/* Define to 1 if you have the `setlocale' function. */
 #define HAVE_SETLOCALE 1
-
-/* Define to 1 if you have the `strtoq' function. */
 #define HAVE_STRTOQ 1
-
-/* Define to 1 if you have the `strtoll' function. */
 #define HAVE_STRTOLL 1
 
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#define LT_OBJDIR ".libs/"
+#define HAVE_ATOMIC_BUILTINS 1
+#define HAVE_SYNC_BUILTINS 1
+#define HAVE_STDATOMIC_H 1
 
-/* Name of package */
-#define PACKAGE "jansson"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "jansson"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "jansson 2.14"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "jansson"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "2.14"
-
-/* The size of `int', as computed by sizeof. */
 #define SIZEOF_INT 4
-
-/* The size of `int32_t', as computed by sizeof. */
 #define SIZEOF_INT32_T 4
-
-/* The size of `int64_t', as computed by sizeof. */
 #define SIZEOF_INT64_T 8
-
-/* The size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 8
-
-/* The size of `long long', as computed by sizeof. */
 #define SIZEOF_LONG_LONG 8
-
-/* The size of `short', as computed by sizeof. */
 #define SIZEOF_SHORT 2
-
-/* The size of `size_t', as computed by sizeof. */
 #define SIZEOF_SIZE_T 8
-
-/* The size of `ssize_t', as computed by sizeof. */
 #define SIZEOF_SSIZE_T 8
-
-/* The size of `uint16_t', as computed by sizeof. */
 #define SIZEOF_UINT16_T 2
-
-/* The size of `uint32_t', as computed by sizeof. */
 #define SIZEOF_UINT32_T 4
-
-/* The size of `uint8_t', as computed by sizeof. */
 #define SIZEOF_UINT8_T 1
 
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
-/* Version number of package */
+#define PACKAGE_NAME "jansson"
+#define PACKAGE_VERSION "2.14"
+#define PACKAGE_STRING "jansson 2.14"
 #define VERSION "2.14"
 
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
-
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef ssize_t */
-
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
-
-/* Define to `__inline__' or `__inline' if that's what the C compiler
-   calls it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-/* #undef inline */
-#endif
-
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef pid_t */
-
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
-
-/* Define to `int' if <sys/types.h> does not define. */
-/* #undef ssize_t */
-
-/* Define to the type of a signed integer type of width exactly 16 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef int16_t */
-
-/* Define to the type of a signed integer type of width exactly 32 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef int32_t */
-
-/* Define to the type of a signed integer type of width exactly 64 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef int64_t */
-
-/* Define to the type of a signed integer type of width exactly 8 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef int8_t */
-
-/* Define to the type of an unsigned integer type of width exactly 16 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef uint16_t */
-
-/* Define to the type of an unsigned integer type of width exactly 32 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef uint32_t */
-
-/* Define to the type of an unsigned integer type of width exactly 64 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef uint64_t */
-
-/* Define to the type of an unsigned integer type of width exactly 8 bits if
-   such a type exists and the standard includes do not define it. */
-/* #undef uint8_t */
-
-/* Define as `fork' if `vfork' exists but `fork' doesn't. */
-/* #undef vfork */
-
-/* Define to 1 if you have the `_strtoi64' function. */
-/* #undef HAVE__STRTOI64 */
-
-/* Define to 1 if you have the `__atomic_add_fetch' builtin. */
-#define HAVE_ATOMIC_BUILTINS 1
-
-/* Define to 1 if you have the `__sync_add_and_fetch' builtin. */
-#define HAVE_SYNC_BUILTINS 1
-
-/* Define to 1 if you have the <stdatomic.h> header file. */
-#define HAVE_STDATOMIC_H 1
+#define STDC_HEADERS 1
+#define LT_OBJDIR ".libs/"
 
 #endif /* JANSSON_CONFIG_HPP */
